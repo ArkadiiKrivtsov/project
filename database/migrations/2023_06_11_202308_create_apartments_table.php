863bbef4c5f5cae8380461f410e55d813e60eb69
ArkadiiKrivtsov <arkadiy059@gmail.com>
2023-06-12 21:55:23 +0300

    first_migration

---
+++ database/migrations/2023_06_11_202308_create_apartments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('rooms');
+            $table->integer('price');
+            $table->integer('square');
+            $table->integer('floor');
+            $table->string('address');
+            $table->string('image_plan');
+            $table->string('image_final_view');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('apartments');
+    }
+};
