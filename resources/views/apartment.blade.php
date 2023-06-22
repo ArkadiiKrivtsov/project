798f1152fc0a0ec7ea4234c730547b1702e21d19
ArkadiiKrivtsov <arkadiy059@gmail.com>
2023-06-22 22:54:04 +0300

    editHeader&Footer

--- resources/views/apartment.blade.php
+++ resources/views/apartment.blade.php
@@ -1,37 +1,41 @@
-@include('layout.header')
+@extends('layout.layout')
 
+   @section('title') Главная @endsection
+
+      @section('content')
+      
       <div class="slider middle">
-        <div class="slides">
-           <input type="radio" name="r" id="r1" checked>
-           <input type="radio" name="r" id="r2">
-           <div class="slide s1">
-              <img src="{{ asset('assets/images/demo/' . $apartment->image_final_view) }}" alt="Изображение" class="img">
-           </div>
-           <div class="slide">
-              <img src="{{ asset('assets/images/demo/' . $apartment->image_plan) }}" alt="Изображение" class="img">
-           </div>
-        </div>
-        <div class="navigation">
-           <label for="r1" class="bar"></label>
-           <label for="r2" class="bar"></label>
-        </div>
-     </div>
-
-    <button type="button" class="inline-block bg-red-600 hover:bg-opacity-70 focus:outline-none text-white font-bold py-2 px-4 rounded">
-      Купить
-    </button>
-    
-    <button type="button" class="inline-block bg-red-600 hover:bg-opacity-70 focus:outline-none text-white font-bold py-2 px-4 rounded">
-      Оформить ипотеку
-    </button>
-
-    <div class="apartment_discribe">
-        <h>
-            <p style="text-indent: 25px;"><?=htmlspecialchars($apartment->description)?></p>
-        </h>
-        <p>Цена: <?=htmlspecialchars($apartment->price)?> RUB</p>
-        <p>Площадь: <?=htmlspecialchars($apartment->square)?> м²</p> 
-        <p>Этаж: <?=htmlspecialchars($apartment->floor)?></p>  
-    </div>
+         <div class="slides">
+            <input type="radio" name="r" id="r1" checked>
+            <input type="radio" name="r" id="r2">
+            <div class="slide s1">
+               <img src="{{ asset('assets/images/demo/' . $apartment->image_final_view) }}" alt="Изображение" class="img">
+            </div>
+            <div class="slide">
+               <img src="{{ asset('assets/images/demo/' . $apartment->image_plan) }}" alt="Изображение" class="img">
+            </div>
+         </div>
+         <div class="navigation">
+            <label for="r1" class="bar"></label>
+            <label for="r2" class="bar"></label>
+         </div>
+      </div>
+ 
+      <button type="button" class="inline-block bg-red-600 hover:bg-opacity-70 focus:outline-none text-white font-bold py-2 px-4 rounded">
+       Купить
+      </button>
      
-@include('layout.footer')
+      <button type="button" class="inline-block bg-red-600 hover:bg-opacity-70 focus:outline-none text-white font-bold py-2 px-4 rounded">
+       Оформить ипотеку
+      </button>
+ 
+      <div class="apartment_discribe">
+         <h>
+             <p style="text-indent: 25px;"><?=htmlspecialchars($apartment->description)?></p>
+         </h>
+         <p>Цена: <?=htmlspecialchars($apartment->price)?> RUB</p>
+         <p>Площадь: <?=htmlspecialchars($apartment->square)?> м²</p> 
+         <p>Этаж: <?=htmlspecialchars($apartment->floor)?></p>  
+      </div>
+
+   @endsection
