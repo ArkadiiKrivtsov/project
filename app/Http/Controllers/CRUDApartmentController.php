2dd09fb41edbe1d2051e60a3f272040f03109f05
ArkadiiKrivtsov <arkadiy059@gmail.com>
2023-07-04 15:51:19 +0300

    added function update

--- app/Http/Controllers/CRUDApartmentController.php
+++ app/Http/Controllers/CRUDApartmentController.php
@@ -38,4 +38,31 @@ class CRUDApartmentController extends Controller
         $apartment->delete();
         return redirect('/admin_section');
     }
+
+    public function updateForm($id)
+    {
+        $apartment = Apartment::find($id);
+
+        return view('update_apartment', ['apartment' => $apartment]);
+    }
+
+    public function update(Request $request)
+    {
+        $input = $request->all();
+        
+        $apartment = Apartment::find($input['id']);
+
+        isset($input['address']) ? $apartment->update(['address' => $input['address']]) : $apartment->address;
+        isset($input['rooms']) ? $apartment->update(['rooms' => $input['rooms']]) : $apartment->rooms;
+        isset($input['price']) ? $apartment->update(['price' => $input['price']]) : $apartment->price;
+        isset($input['square']) ? $apartment->update(['square' => $input['square']]) : $apartment->square;
+        isset($input['floor']) ? $apartment->update(['floor' => $input['floor']]) : $apartment->floor;
+        isset($input['description']) ? $apartment->update(['description' => $input['description']]) : $apartment->description;
+        isset($input['image_final_view']) ? $apartment->update(['image_final_view' => $input['image_final_view']->store('uploads', 'for_images')]) : $apartment->image_final_view;
+        isset($input['image_plan']) ? $apartment->update(['image_plan' => $input['image_plan']->store('uploads', 'for_images')]) : $apartment->image_plan;
+        
+        return redirect('/admin_section');
+
+    }
 }
+
