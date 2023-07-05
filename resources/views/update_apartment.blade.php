8ffc6fd3084ba71d6da119c64806bc7b0a09adf1
ArkadiiKrivtsov <arkadiy059@gmail.com>
2023-07-05 15:01:28 +0300

    edit update function

--- resources/views/update_apartment.blade.php
+++ resources/views/update_apartment.blade.php
@@ -9,35 +9,35 @@
                 <h2> Укажите новые данные квартиры </h2>
                 
                     <div class="mt-8 max-w-md">
-                        <form method="post" action="/update" enctype="multipart/form-data">
+                        <form method="post" action="/update/{{$apartment->id}}" enctype="multipart/form-data">
                             {{ csrf_field() }}
+                            @method('PUT')
+
                             <div class="grid grid-cols-1 gap-6">
-    
-                            <input type="hidden" name="id" value="{{$apartment->id}}">
-                            
+                                
                             <div class="block">
                                 <label for="field1" class="py-4 text-gray-700 font-bold">Введите новый адресс</label>
-                                <input name="address" type="text" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="{{$apartment->address}}">
+                                <input name="address" type="text" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" value="{{$apartment->address}}">
                             </div>
 
                             <div class="block">
                                 <label for="field1" class="py-4 text-gray-700 font-bold">Укажите новую стоимость</label>
-                                <input name="price" type="number" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="{{$apartment->price}}">
+                                <input name="price" type="number" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" value="{{$apartment->price}}">
                             </div>
 
                             <div class="block">
                                 <label for="field1" class="py-4 text-gray-700 font-bold">Укажите кол-во комнат</label>
-                                <input name="rooms" type="number" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="{{$apartment->rooms}}">
+                                <input name="rooms" type="number" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" value="{{$apartment->rooms}}">
                             </div>
 
                             <div class="block">
                                 <label for="field1" class="py-4 text-gray-700 font-bold">Укажите площадь (м²)</label>
-                                <input name="square" type="number" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="{{$apartment->square}}">
+                                <input name="square" type="number" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" value="{{$apartment->square}}">
                             </div>
 
                             <div class="block">
                                 <label for="field1" class="py-4 text-gray-700 font-bold">Укажите этаж</label>
-                                <input name="floor" type="number" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="{{$apartment->floor}}">
+                                <input name="floor" type="number" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" value="{{$apartment->floor}}">
                             </div>
 
                             <div class="block">
@@ -52,7 +52,7 @@
 
                             <div class="block">
                                 <label for="field6" class="py-4 text-gray-700">Добавить описание (не более 255 символов)</label>
-                                <textarea name="description" type="text" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" rows="3" placeholder="{{$apartment->description}}"></textarea>
+                                <textarea name="description" type="text" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" rows="3">{{$apartment->description}}</textarea>
                             </div>
     
                             <div class="block">
