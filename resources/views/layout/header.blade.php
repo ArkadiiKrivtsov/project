8c6a4e9423f4f53b56ecab62df428628fa0e7415
ArkadiiKrivtsov <arkadiy059@gmail.com>
2023-07-07 01:45:55 +0300

    handled crud exceptions

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,60 +1,81 @@
 <header class="header yolo-header-style-1">
-        <!-- самый верх шапки -->
-        <div class="yolo-top-bar">
-          <div class="container">
+    <!-- самый верх шапки -->
+    <div class="yolo-top-bar">
+        <div class="container">
             <div class="row">
-              <div class="top-sidebar top-bar-left col-md-4">
-                <aside id="text-11" class="widget widget_text">
-                  <div class="textwidget">
-                    <div>Добро пожаловать на купидом.ру!</div>
-                  </div>
-                </aside>
-              </div>
-              <div class="top-sidebar top-bar-right col-md-8">
-                <aside id="text-6" class="widget widget_text">
-                  <div class="textwidget"><i class="fa fa-home"></i> ул. Пушкина д.59, Москва <i style="margin-left: 15px" class="fa fa-phone"></i> 8800-777-55-55</div>
-                </aside>
-              </div>
+                <div class="top-sidebar top-bar-left col-md-4">
+                    <aside id="text-11" class="widget widget_text">
+                        <div class="textwidget">
+                            <div>Добро пожаловать на купидом.ру!</div>
+                        </div>
+                    </aside>
+                </div>
+                <div class="top-sidebar top-bar-right col-md-8">
+                    <aside id="text-6" class="widget widget_text">
+                        <div class="textwidget"><i class="fa fa-home"></i> ул. Пушкина д.59, Москва <i
+                                style="margin-left: 15px" class="fa fa-phone"></i> 8800-777-55-55</div>
+                    </aside>
+                </div>
             </div>
-          </div>
         </div>
-        <!-- лого и войти -->
-        <div class="yolo-header-top yolo-header-top-style-1">
-          <div class="container">
+    </div>
+    <!-- лого и войти -->
+    <div class="yolo-header-top yolo-header-top-style-1">
+        <div class="container">
             <div class="row">
-              <div class="col-md-4">
-                <div class="header-logo">
-                  <h1><a href="/"><img src="{{ asset('assets/images/logo/logo.png') }}" alt="logo"/></a></h1>
+                <div class="col-md-4">
+                    <div class="header-logo">
+                        <h1><a href="/"><img src="{{ asset('assets/images/logo/logo.png') }}"
+                                    alt="logo" /></a></h1>
+                    </div>
                 </div>
-              </div>
-              <div class="col-md-8">
-                <div class="header-customize header-customize-right">
-                  <a href="/login.php">
-                  <div class="custom-text-wrapper header-customize-item"><i class='fa fa-check-square'></i></i>Войти</div>
-                  </div>
+                <div class="col-md-8">
+                    <div class="header-customize header-customize-right">
+                        <a href="/login.php">
+                            <div class="custom-text-wrapper header-customize-item"><i
+                                    class='fa fa-check-square'></i></i>Войти</div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
-        <!-- меню -->
-        <div class="header-bottom">
-          <div class="container">
+    </div>
+    </div>
+    <!-- меню -->
+    <div class="header-bottom">
+        <div class="container">
             <div class="main-nav-wrapper">
-              <div class="header-left">
-                <nav  class="main-nav">
-                  <ul class="nav">
-                    <li class="active menu-item menu-home"><a href="/">Главная</a>
-                    </li>
-                    <li class="menu-item menu-blog"><a href="/catalog">Каталог</a>
-                    </li>
-                    <li class="menu-item menu-blog"><a href="/admin_section">Административный раздел</a>
-                    </li>
-                  </ul>
-                </nav>
-                <!-- .header-main-nav-->
-              </div>
+                <div class="header-left">
+                    <nav class="main-nav">
+                        <ul class="nav">
+                            <li class="active menu-item menu-home"><a href="/">Главная</a>
+                            </li>
+                            <li class="menu-item menu-blog"><a href="/catalog">Каталог</a>
+                            </li>
+                            <li class="menu-item menu-blog"><a href="/admin_section">Административный раздел</a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <!-- .header-main-nav-->
+                </div>
             </div>
-          </div>
         </div>
+    </div>
+
+    <div align="center" style="padding-top:30px;">
+
+        @if (session()->has('message'))
+            <div class="alert alert-success">
+                {{ session()->get('message') }}
+            </div>
+        @endif
+
+        @if (session()->has('error'))
+            <div class="alert alert-danger">
+                {{ session()->get('error') }}
+            </div>
+        @endif
+
+    </div>
+
+
 </header>
